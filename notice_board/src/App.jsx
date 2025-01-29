4adb1ef8bb50b5caaeb086c4bb904c37822bbe72
hawi-me <hawiabdi41@gmail.com>
2025-01-29 17:04:47 +0400

    development

--- notice_board/src/App.jsx
+++ notice_board/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+// import {BrowserRouter, Routes, Route}    from 'react-router-dom'
+// import Signup from './pages/Authentication/sign-up'
+// import { Home } from '@mui/icons-material'
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home/>} />
+//         <Route path="/notice/:id" element={<Notice />} />
+//         <Route path="/admin" element={<AdminDashboard />}/>
+//         <Route path="/create-post" element={<CreatePost />} />
+//         <Route path="/manage-post" element={<ManagePost />} />
+//         <Route path="/manage-comment" element={<Managecomment />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Signup/>} />
+//         <Route path="/" element={<Forget/>} />
 
-export default function App() {
-  return (
-    <div>
-    <h1>the first page </h1>
-      
-    </div>
-  )
-}
+        
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }   
